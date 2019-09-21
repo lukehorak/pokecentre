7d7b199b58ea785f1c180330c8e9ca2d9e065fdf
Lukas Horak <lukasbhorak@gmail.com>
2019-09-21 12:00:17 -0400

    updated db population script

--- api/getData.js
+++ api/getData.js
@@ -5,12 +5,27 @@ const promises = [];
 
 //then try 808
 
+const getStats = (block) => {
+  const baseStats = {};
+  for (let stat of block) {
+    baseStats[stat.stat.name] = stat.base_stat
+  }
+  return baseStats;
+}
+
 for (let i = 1; i < 10; i++) {
   promises.push(pokedex.resource(`/api/v2/pokemon/${i}`));
 }
 
+
 Promise.all(promises).then(data => {
-    const result = data.map(pokemon => pokemon.name);
+    const result = data.map(pokemon => ({
+      name: pokemon.name,
+      species: pokemon.species.name,
+      types: pokemon.types.map( type => type.type.name ),
+      stats: getStats(pokemon.stats)
+
+    }));
     console.log(result)
   })
   .catch(error => {
