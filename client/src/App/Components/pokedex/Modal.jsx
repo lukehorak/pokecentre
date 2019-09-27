f56ef2cce1033d4de95c1a9ad2db084803412eba
Lukas Horak <lukasbhorak@gmail.com>
2019-09-27 12:06:23 -0400

    moved chart into separate component

--- client/src/App/Components/pokedex/Modal.jsx
+++ client/src/App/Components/pokedex/Modal.jsx
@@ -1,91 +1,10 @@
 import React, { Component } from 'react';
 import TypeList from './Types';
 import Sprite from './Sprite';
+import Graph from './Graph';
 import { capitalize } from '../../scripts/pokedexlib';
 
-
-import Chart from 'react-apexcharts';
-
 class Modal extends Component {
-
-  getColor = (value) => {
-    if (value > 140){
-      return "#28E2AA"
-    }
-    if (value > 100){
-      return "#75B200";
-    }
-    if (value > 90){
-      return "#a9dd5a";
-    }
-    if (value > 60){
-      return "#F8BC4C"
-    }
-    return "#E56565";
-  }
-
-  constructor(props) {
-    super(props);
-
-    const { hp, speed, attack, defense, special_attack, special_defense } = this.props.pokemon;
-
-    this.state = {
-      options: {
-        chart: {
-          animations: {
-            enabled: true,
-            easing: 'easeinout',
-            speed: 900,
-            animateGradually: {
-                enabled: true,
-                delay: 450
-            },
-            dynamicAnimation: {
-              enabled: true,
-              speed: 800
-            }
-          }
-        },
-        colors: [
-          this.getColor(hp),
-          this.getColor(speed),
-          this.getColor(attack),
-          this.getColor(defense),
-          this.getColor(special_attack),
-          this.getColor(special_defense)
-        ],
-        xaxis: {
-          categories: ["HP", "Speed", "Attack", "Defense", "Special Attack", "Special Defense"],
-          labels: {
-            show: true,
-            style: {
-              colors: [],
-              fontSize: '12px',
-              fontFamily: 'Helvetica, Arial, sans-serif',
-              cssClass: 'apexcharts-xaxis-label',
-            },
-          }
-        },
-        yaxis: {
-          forceNiceScale: false,
-          min: 0,
-          max: 260
-        },
-        plotOptions: {
-          bar: {
-            barHeight: '90%',
-            distributed: true,
-            horizontal: true,
-          }
-        },
-      },
-      series: [
-        {
-          data: [hp, speed, attack, defense, special_attack, special_defense]
-        }
-      ]
-    }
-  };
   
   render() {
 
@@ -102,12 +21,7 @@ class Modal extends Component {
           <div className="entry-right-side">
             <h1 className="entry-name">{capitalize(species)}</h1>
             <TypeList types={types} species={species}/>
-            <Chart className={"stat-chart"}
-              options={this.state.options}
-              series={this.state.series}
-              type="bar"
-              width="450"
-            />
+            <Graph pokemon={this.props.pokemon} />
           </div>
         </div>
       </div>
