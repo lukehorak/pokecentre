09449c90fda5f5808520c1d503eb5ac9a9e34b65
Lukas Horak <lukasbhorak@gmail.com>
2019-12-02 14:04:18 -0500

    adding passpord

--- db/migrations/20191128155909_users_db.js
+++ db/migrations/20191128155909_users_db.js
@@ -1,16 +1,17 @@
 
-exports.up = function(knex, Promise) {
-  return knex.schema.createTable('users', function(table) {
+exports.up = (knex, Promise) => {
+  return knex.schema.createTable('users', (table) => {
     table.increments('id').unsigned().primary();
-    table.dateTime('createdAt').notNull();
+    table.dateTime('createdAt').notNull().defaultTo(knex.raw('now()'));
     table.dateTime('updatedAt').nullable();
     table.dateTime('deletedAt').nullable();
+    table.boolean('admin').notNullable().defaultTo(false)
 
     table.string('email');
     table.string('password');
   })
 };
 
-exports.down = function(knex, Promise) {
-  return  
+exports.down = (knex, Promise) => {
+  return  knex.schema.dropTable('users');
 };
--- index.js
+++ index.js
@@ -1,15 +1,56 @@
 const express = require('express');
 const path = require('path');
 const busboy = require('connect-busboy');
+const passport = require('passport');
+
+// TODO - finish user auth config
+
+// knex
+const knexConfig  = require("./knexfile");
+const knex        = require("knex")(knexConfig[ENV]);
+const knexLogger  = require('knex-logger');
 
 // Require Routes
 const uploadRoute = require('./api/routes/uploadRoute');
 const testDataRoute = require('./api/routes/testData');
 const pokedexRoute = require('./api/routes/pokedex');
 
+// Configs
 const app = express();
 app.use(express.static(path.join(__dirname, 'client/build')));
 app.use(busboy());
+// Passport
+app.use(session({
+  secret: process.env.SECRET_KEY,
+  resave: false,
+  saveUninitialized: true
+}))
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
+
+passport.serializeUser(function(user, done){
+  done(null, user.id);
+
+});
+passport.deserializeUser(function(id, cb){
+  knex('users')
+    .where({id: id})
+    .then(([user]) => {
+      if (!user) { done (new Error('User not found, Whoops! id:' + id))}  
+      done(null, user);
+    })
+})
+
+// Session
+app.use(session({
+  secret: process.env.SECRET_KEY, //GENERATE SECRET
+  resave: true,
+  saveUninitialized: true,
+  store: new FileStore({path: './data/tmp/session'})
+}))
+app.use(passport.initialize())
+app.use(passport.session())
 
 // Use Routes
 app.use('/api/upload', uploadRoute);
