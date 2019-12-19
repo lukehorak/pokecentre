a10161759df25e33b3e6a0f5c0bf8792e2c0cb34
Lukas Horak <lukasbhorak@gmail.com>
2019-12-19 10:37:34 -0500

    created login form file

---
+++ client/src/App/Components/Login/LoginForm.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+// TODO - build this form
+
+class LoginForm extends Component {
+
+  render() {
+
+    return (
+      <div></div>
+    )
+  }
+}
+
+export default LoginForm;
