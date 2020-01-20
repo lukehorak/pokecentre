a68ec1507f73ad93eb7039c2764c94a4028bc0d4
Lukas Horak <lukasbhorak@gmail.com>
2020-01-20 13:56:37 -0500

    added auth routes skeleton

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -5,4 +5,38 @@ router.get('/', (req, res) => {
   res.json({"Todo": "make this work"});
 })
 
+router.post('/register', (req, res) => {
+  if(!req.body.username || !req.body.password){
+    res.status(400).send({msg: "Need a username AND a password. Try again, champ"});
+  }
+  else {
+    /* 
+    // Register
+    */
+  }
+})
+
+router.post('/login', (req, res) => {
+  if(!req.body.username || !req.body.password){
+    res.status(400).send({msg: "Need a username AND a password. Try again, champ"});
+  }
+  else {
+    /* 
+    // Do the Login thing
+    */
+  }
+})
+
+router.get('/logout', (req, res) => {
+  if(!req.body.username){
+    res.status(400).send({msg: "Can't log 'nobody' out. Try again old sport"});
+  }
+  else {
+    /* 
+    // Do the Login thing
+    */
+  }
+})
+
+
 module.exports = router;
